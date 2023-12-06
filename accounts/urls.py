5db8cbfc56245b6e06eeae3fd1d87c3a2d533952
professorfernanda <fmurillo@sdgku.edu>
2023-12-05 16:44:40 -0800

    No Status

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+]
---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from posts import views
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name="post_list"),
+    path("new/", views.PostCreateView.as_view(), name="post_new"),
+    path("<int:pk>/", views.PostDetailView.as_view(), name="post_detail"),
+    path("<int:pk>/edit/", views.PostUpdateView.as_view(), name="post_edit"),
+    path("<int:pk>/delete/", views.PostDeleteView.as_view(), name="post_delete"),
+]
